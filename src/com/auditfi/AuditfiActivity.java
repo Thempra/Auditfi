7350bb6374f1177c666a545bb4a7712dd3004f3b
Thempra <thempra@ieee.org>
2011-08-23 16:24:46 +0000

    * Incluida traducción en ingles * Posibilidad de copiar en el portapapeles la clave * Mejora de la interfaz para dispositivos en portrait y landscape

--- src/com/auditfi/AuditfiActivity.java
+++ src/com/auditfi/AuditfiActivity.java
@@ -2,7 +2,11 @@ package com.auditfi;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Vector;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -12,25 +16,33 @@ import android.content.DialogInterface;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.text.ClipboardManager;
 import android.text.Html;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class AuditfiActivity extends Activity implements OnClickListener {
 	private static final String TAG = "Auditfi";
 	WifiManager wifi;
 	BroadcastReceiver receiver;
 
-	TextView textStatus;
+	
 	Button buttonScan;
 	CheckBox vulnerables;
 
@@ -45,7 +57,7 @@ public class AuditfiActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.main);
 
 		// Setup UI
-		textStatus = (TextView) findViewById(R.id.textStatus);
+		
 		vulnerables = (CheckBox) findViewById(R.id.vulnerables);
 		buttonScan = (Button) findViewById(R.id.buttonScan);
 		buttonScan.setOnClickListener(this);
@@ -68,7 +80,7 @@ public class AuditfiActivity extends Activity implements OnClickListener {
 	public void onClick(View view) {
 
 		if (view.getId() == R.id.buttonScan) {
-			
+			ListView list = (ListView) findViewById(R.id.filaWifis);
 			
 			if(!wifi.isWifiEnabled()){
 				//No esta activado el wifi!
@@ -82,11 +94,70 @@ public class AuditfiActivity extends Activity implements OnClickListener {
 			wifi.startScan();
 
 			// Listado de las redes wifi
-			List<ScanResult> configs = wifi.getScanResults();
+			final List<ScanResult> configs = wifi.getScanResults();
 			
-
-			textStatus.setText(""); //Limpiamos el texto
+			//textStatus.setText(""); //Limpiamos el texto
 			int numero_redes = 0;
+			
+			
+			
+			/******************************************************************/
+			/******** EVENTO CLICK EN EL LISTADO DE LAS WIFIS ****************/
+			/******************************************************************/
+
+			OnItemClickListener clickListaWifis= new OnItemClickListener() {
+				public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+						long arg3) {
+
+					//Si esta marcado solo ver vulnerables tenemos que buscar cual ha sido
+					if (vulnerables.isChecked())
+					{
+						int cont=0;
+						for (ScanResult config : configs) 
+						{
+							
+							if (config.SSID.contains("JAZZTEL_") || config.SSID.contains("WLAN_")) 
+							{
+								arg2--;
+							}
+							
+							if (arg2<0)
+							{
+								arg2=cont;
+								break;
+							}
+							cont++;
+						}
+					}
+					
+					//Identificamos
+					String nombre = "";
+					if (configs.get(arg2).SSID.length()==12) //Tiene 12 caracteres, puede ser JAZZTEL_XXXX
+						nombre=configs.get(arg2).SSID.substring(0, 8);
+					else if (configs.get(arg2).SSID.length()==9) //Tiene 9 caracteres, puede ser WLAN_XXXX
+					    nombre=configs.get(arg2).SSID.substring(0, 5);
+					
+					if (nombre.equals("JAZZTEL_") || nombre.equals("WLAN_")) 
+					{
+						String key=getKey(configs.get(arg2).SSID, configs.get(arg2).BSSID);
+						
+						/*Copiamos al portapapeles */
+						ClipboardManager clipboard = 
+							      (ClipboardManager) getSystemService(CLIPBOARD_SERVICE); 
+							 clipboard.setText(key);
+						
+						showMsg(getResources().getString(R.string.Copiar),getResources().getString(R.string.contrasenaCopiada)+" "+key, getResources().getString(R.string.Aceptar));
+						
+					}else
+					{
+						showMsg(getResources().getString(R.string.Copiar),getResources().getString(R.string.redNoVulnerable), getResources().getString(R.string.Aceptar));
+					}
+					
+					
+				}
+			};
+			
+			/*
 			for (ScanResult config : configs) {
 		
 				String nombre = "";
@@ -107,15 +178,82 @@ public class AuditfiActivity extends Activity implements OnClickListener {
 					textStatus.append("\n\n");
 				}
 			}
+			*/
 			
-			//Si no se ha mostrado ninguna red, escribimos mensaje
-			if(numero_redes == 0){
-				textStatus.setText(getResources().getString( R.string.NoHayRedes ));
-			}
+			
+
+			
+				// Lo asignamos a la vista
+				ArrayList<HashMap<String, String>> mylist = new ArrayList<HashMap<String, String>>();
+				HashMap<String, String> map;// = new HashMap<String, String>();
+				
+				
+				
+				for (ScanResult config : configs) {
+					map = new HashMap<String, String>();
+					
+					String nombre = "";
+					if (config.SSID.length()==12) //Tiene 12 caracteres, puede ser JAZZTEL_XXXX
+						nombre=config.SSID.substring(0, 8);
+					else if (config.SSID.length()==9) //Tiene 9 caracteres, puede ser WLAN_XXXX
+					    nombre=config.SSID.substring(0, 5);
+					
+					//Si esta marcado el checkbox vulnerables, solamente cogemos las jazztel y las wlan
+					if (!vulnerables.isChecked() || nombre.equals("JAZZTEL_") || nombre.equals("WLAN_")){
+
+						map.put("ssid", config.SSID);
+						map.put("bssid",  "MAC: " + config.BSSID);
+						// map.put("current",Integer.toString(bk.getCurrentTime()));
+						map.put("level", config.level +" dB");
+
+						numero_redes++;
+						//Si son JAZZTEL_XXXX o WLAN_XXXX, mostramos la clave
+						if (nombre.equals("JAZZTEL_") || nombre.equals("WLAN_")) {
+							map.put("key", "Key: "+ getKey(config.SSID, config.BSSID));
+
+						}
+						
+
+						// map.put("total",Integer.toString(bk.getTotal()));
+						
+						mylist.add(map);
+					}
+					
+					
+					
+				
+				}
+
+				//Si no se ha mostrado ninguna red, escribimos mensaje
+				if(numero_redes == 0){
+					map = new HashMap<String, String>();
+					map.put("ssid", getResources().getString( R.string.NoHayRedes ));
+					mylist.add(map);
+					
+				}
+
+				SimpleAdapter mSchedule = new SimpleAdapter(this, mylist,
+						R.layout.filawifis,
+						new String[] { "ssid", "bssid", "level", "key"}, new int[] {
+								R.id.SSID_CELL, R.id.BSSID_CELL, R.id.LEVEL_CELL, R.id.KEY_CELL });
+				list.setAdapter(mSchedule);
+
+				
+				// creamos el overwrite el click
+				list.setOnItemClickListener(clickListaWifis);
+
+				// creamos el handler del longCLick
+				//list.setOnItemLongClickListener(longClickListaWifis);
+				
+				
+			
 			
 			Log.d(TAG, "onClick() Refresh()");
 
 		}
+		
+		
+	
 
 	}
 
@@ -214,4 +352,6 @@ public class AuditfiActivity extends Activity implements OnClickListener {
 				} 
 	        }).show();
 	    }
+	 
+	 
 }
