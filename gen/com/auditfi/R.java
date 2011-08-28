ab41c10e43896d34604f1dce09b195426650f5a7
Jose Carlos Temprado <thempra@ieee.org>
2011-08-28 13:32:24 +0200

    Traducciones a Portugues, Italiano, Aleman y frances. Inclusion de Licencia

--- gen/com/auditfi/R.java
+++ gen/com/auditfi/R.java
@@ -15,34 +15,42 @@ public final class R {
         public static final int background=0x7f020001;
         public static final int exit=0x7f020002;
         public static final int icon=0x7f020003;
-        public static final int wifi=0x7f020004;
+        public static final int signal=0x7f020004;
+        public static final int wifi=0x7f020005;
     }
     public static final class id {
+        public static final int BSSID_CELL=0x7f06000b;
         public static final int Disclaimer1=0x7f060001;
         public static final int Disclaimer2=0x7f060002;
         public static final int Disclaimer3=0x7f060003;
         public static final int EscanearImagen=0x7f060004;
         public static final int EscanearLabel=0x7f060005;
+        public static final int KEY_CELL=0x7f060009;
+        public static final int LEVEL_CELL=0x7f06000a;
+        public static final int SSID_CELL=0x7f060008;
         public static final int SalirImagen=0x7f060006;
         public static final int SalirLabel=0x7f060007;
-        public static final int ScrollView01=0x7f06000a;
         public static final int TituloLabel=0x7f060000;
-        public static final int about=0x7f06000c;
-        public static final int buttonScan=0x7f060009;
-        public static final int exit=0x7f06000d;
-        public static final int textStatus=0x7f06000b;
-        public static final int vulnerables=0x7f060008;
+        public static final int about=0x7f060010;
+        public static final int buttonScan=0x7f06000c;
+        public static final int exit=0x7f060011;
+        public static final int filaWifis=0x7f06000f;
+        public static final int tabWifis=0x7f06000e;
+        public static final int vulnerables=0x7f06000d;
     }
     public static final class layout {
         public static final int disclaimer=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int filawifis=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class menu {
         public static final int auditfi_menu=0x7f050000;
     }
     public static final class string {
+        public static final int Aceptar=0x7f040013;
         public static final int ActivandoWifi=0x7f04000b;
         public static final int BuscarRedes=0x7f040009;
+        public static final int Copiar=0x7f040010;
         public static final int Disclaimer1=0x7f040005;
         public static final int Disclaimer2=0x7f040006;
         public static final int Disclaimer3=0x7f040007;
@@ -56,6 +64,8 @@ public final class R {
         public static final int WifiOff=0x7f04000a;
         public static final int about=0x7f04000e;
         public static final int app_name=0x7f040000;
+        public static final int contrasenaCopiada=0x7f040011;
         public static final int exit=0x7f04000f;
+        public static final int redNoVulnerable=0x7f040012;
     }
 }
